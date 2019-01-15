c3646f5343cc92e8df6a3f3743a2c97b7ae5b4eb
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2019-01-15 15:35:59 +0300

    [NI] Discriminate constraints with `Nothing(?)` lower bounds

---
+++ compiler/resolution/src/org/jetbrains/kotlin/resolve/calls/inference/components/TrivialConstraintTypeInferenceOracle.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.resolve.calls.inference.components
+
+import org.jetbrains.kotlin.resolve.calls.inference.model.Constraint
+import org.jetbrains.kotlin.resolve.calls.inference.model.ConstraintKind
+import org.jetbrains.kotlin.types.UnwrappedType
+import org.jetbrains.kotlin.types.typeUtil.isNothing
+import org.jetbrains.kotlin.types.typeUtil.isNullableNothing
+
+class TrivialConstraintTypeInferenceOracle {
+    fun isTrivialConstraint(constraint: Constraint): Boolean {
+        // TODO: probably we also can take into account `T <: Any(?)` constraints
+        return constraint.kind == ConstraintKind.LOWER && constraint.type.isNothingOrNullableNothing()
+    }
+}
+
+private fun UnwrappedType.isNothingOrNullableNothing(): Boolean =
+    isNothing() || isNullableNothing()
