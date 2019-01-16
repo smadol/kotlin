59fc22bfbba835460c435dec1ad3e058a7f8dbbf
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-01-16 13:45:39 +0300

    Make FIR expression stub not deprecated
    Probably it will be still used in stub mode,
    when we need only declarations

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/expressions/impl/FirExpressionStub.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/expressions/impl/FirExpressionStub.kt
@@ -9,5 +9,4 @@ import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.expressions.FirExpression
 
-@Deprecated("Temporary class until we have normal expressions")
 class FirExpressionStub(override val session: FirSession, override val psi: PsiElement?) : FirExpression
