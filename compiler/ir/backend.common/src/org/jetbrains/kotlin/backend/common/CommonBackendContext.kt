d1e16466bca1788ef1e5d8c0387948545e306909
Georgy Bronnikov <georgy.bronnikov@jetbrains.com>
2019-02-02 13:25:24 +0300

    Restore `shouldBeDumped()` in compiler phases

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/CommonBackendContext.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/CommonBackendContext.kt
@@ -6,6 +6,7 @@
 package org.jetbrains.kotlin.backend.common
 
 import org.jetbrains.kotlin.backend.common.ir.Ir
+import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.ir.IrElement
@@ -29,4 +30,6 @@ interface CommonBackendContext : BackendContext, LoggingContext {
     fun getInternalFunctions(name: String): List<FunctionDescriptor>
 
     fun report(element: IrElement?, irFile: IrFile?, message: String, isError: Boolean)
+
+    val configuration: CompilerConfiguration
 }
