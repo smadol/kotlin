541b503ddc5114e10f203b37753277448d4630ff
Andrey Uskov <Andrey.Uskov@jetbrains.com>
2019-02-18 13:45:01 +0300

    Minor. Reformat KotlinGradleModelBuilder

--- idea/kotlin-gradle-tooling/src/KotlinGradleModelBuilder.kt
+++ idea/kotlin-gradle-tooling/src/KotlinGradleModelBuilder.kt
@@ -35,9 +35,9 @@ interface ArgsInfo : Serializable {
 }
 
 class ArgsInfoImpl(
-        override val currentArguments: List<String>,
-        override val defaultArguments: List<String>,
-        override val dependencyClasspath: List<String>
+    override val currentArguments: List<String>,
+    override val defaultArguments: List<String>,
+    override val dependencyClasspath: List<String>
 ) : ArgsInfo
 
 typealias CompilerArgumentsBySourceSet = Map<String, ArgsInfo>
@@ -52,23 +52,25 @@ interface KotlinGradleModel : Serializable {
 }
 
 class KotlinGradleModelImpl(
-        override val hasKotlinPlugin: Boolean,
-        override val compilerArgumentsBySourceSet: CompilerArgumentsBySourceSet,
-        override val coroutines: String?,
-        override val platformPluginId: String?,
-        override val implements: List<String>,
-        override val kotlinTarget: String? = null
+    override val hasKotlinPlugin: Boolean,
+    override val compilerArgumentsBySourceSet: CompilerArgumentsBySourceSet,
+    override val coroutines: String?,
+    override val platformPluginId: String?,
+    override val implements: List<String>,
+    override val kotlinTarget: String? = null
 ) : KotlinGradleModel
 
 abstract class AbstractKotlinGradleModelBuilder : ModelBuilderService {
     companion object {
-        val kotlinCompileTaskClasses = listOf("org.jetbrains.kotlin.gradle.tasks.KotlinCompile_Decorated",
-                                              "org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile_Decorated",
-                                              "org.jetbrains.kotlin.gradle.tasks.KotlinCompileCommon_Decorated")
+        val kotlinCompileTaskClasses = listOf(
+            "org.jetbrains.kotlin.gradle.tasks.KotlinCompile_Decorated",
+            "org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile_Decorated",
+            "org.jetbrains.kotlin.gradle.tasks.KotlinCompileCommon_Decorated"
+        )
         val platformPluginIds = listOf("kotlin-platform-jvm", "kotlin-platform-js", "kotlin-platform-common")
         val pluginToPlatform = linkedMapOf(
-                "kotlin" to "kotlin-platform-jvm",
-                "kotlin2js" to "kotlin-platform-js"
+            "kotlin" to "kotlin-platform-jvm",
+            "kotlin2js" to "kotlin-platform-js"
         )
         val kotlinPluginIds = listOf("kotlin", "kotlin2js", "kotlin-android")
         val ABSTRACT_KOTLIN_COMPILE_CLASS = "org.jetbrains.kotlin.gradle.tasks.AbstractKotlinCompile"
@@ -76,7 +78,7 @@ abstract class AbstractKotlinGradleModelBuilder : ModelBuilderService {
         fun Task.getSourceSetName(): String {
             return try {
                 javaClass.methods.firstOrNull { it.name.startsWith("getSourceSetName") && it.parameterTypes.isEmpty() }?.invoke(this) as? String
-            } catch (e : InvocationTargetException) {
+            } catch (e: InvocationTargetException) {
                 null // can be thrown if property is not initialized yet
             } ?: "main"
         }
@@ -85,16 +87,17 @@ abstract class AbstractKotlinGradleModelBuilder : ModelBuilderService {
 
 class KotlinGradleModelBuilder : AbstractKotlinGradleModelBuilder() {
     override fun getErrorMessageBuilder(project: Project, e: Exception): ErrorMessageBuilder {
-        return ErrorMessageBuilder.create(project, e, "Gradle import errors").withDescription("Unable to build Kotlin project configuration")
+        return ErrorMessageBuilder.create(project, e, "Gradle import errors")
+            .withDescription("Unable to build Kotlin project configuration")
     }
 
     override fun canBuild(modelName: String?): Boolean = modelName == KotlinGradleModel::class.java.name
 
     private fun getImplementedProjects(project: Project): List<Project> {
         return listOf("expectedBy", "implement")
-                .flatMap { project.configurations.findByName(it)?.dependencies ?: emptySet<Dependency>() }
-                .filterIsInstance<ProjectDependency>()
-                .mapNotNull { it.dependencyProject }
+            .flatMap { project.configurations.findByName(it)?.dependencies ?: emptySet<Dependency>() }
+            .filterIsInstance<ProjectDependency>()
+            .mapNotNull { it.dependencyProject }
     }
 
     // see GradleProjectResolverUtil.getModuleId() in IDEA codebase
@@ -104,8 +107,7 @@ class KotlinGradleModelBuilder : AbstractKotlinGradleModelBuilder() {
     private fun Task.getCompilerArguments(methodName: String): List<String> {
         return try {
             javaClass.getDeclaredMethod(methodName).invoke(this) as List<String>
-        }
-        catch (e : Exception) {
+        } catch (e: Exception) {
             // No argument accessor method is available
             emptyList()
         }
@@ -117,14 +119,11 @@ class KotlinGradleModelBuilder : AbstractKotlinGradleModelBuilder() {
             val getCompileClasspath = abstractKotlinCompileClass.getDeclaredMethod("getCompileClasspath").apply { isAccessible = true }
             @Suppress("UNCHECKED_CAST")
             return (getCompileClasspath.invoke(this) as Collection<File>).map { it.path }
-        }
-        catch(e: ClassNotFoundException) {
+        } catch (e: ClassNotFoundException) {
             // Leave arguments unchanged
-        }
-        catch (e: NoSuchMethodException) {
+        } catch (e: NoSuchMethodException) {
             // Leave arguments unchanged
-        }
-        catch (e: InvocationTargetException) {
+        } catch (e: InvocationTargetException) {
             // We can safely ignore this exception here as getCompileClasspath() gets called again at a later time
             // Leave arguments unchanged
         }
@@ -135,15 +134,13 @@ class KotlinGradleModelBuilder : AbstractKotlinGradleModelBuilder() {
         val kotlinExtension = project.extensions.findByName("kotlin") ?: return null
         val experimentalExtension = try {
             kotlinExtension::class.java.getMethod("getExperimental").invoke(kotlinExtension)
-        }
-        catch(e: NoSuchMethodException) {
+        } catch (e: NoSuchMethodException) {
             return null
         }
 
         return try {
             experimentalExtension::class.java.getMethod("getCoroutines").invoke(experimentalExtension)?.toString()
-        }
-        catch(e: NoSuchMethodException) {
+        } catch (e: NoSuchMethodException) {
             null
         }
     }
@@ -168,12 +165,12 @@ class KotlinGradleModelBuilder : AbstractKotlinGradleModelBuilder() {
         val implementedProjects = getImplementedProjects(project)
 
         return KotlinGradleModelImpl(
-                kotlinPluginId != null || platformPluginId != null,
-                compilerArgumentsBySourceSet,
-                getCoroutines(project),
-                platform,
-                implementedProjects.map { it.pathOrName() },
-                platform ?: kotlinPluginId
+            kotlinPluginId != null || platformPluginId != null,
+            compilerArgumentsBySourceSet,
+            getCoroutines(project),
+            platform,
+            implementedProjects.map { it.pathOrName() },
+            platform ?: kotlinPluginId
         )
     }
 }
