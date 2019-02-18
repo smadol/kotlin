590768b9777954ccee9274514e96a95612892bbf
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2019-02-18 22:54:28 +0300

    fixup! Preprocess script dependencies before running UnusedSymbolInspection
    ^KT-29474

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/search/searchUtil.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/search/searchUtil.kt
@@ -19,9 +19,7 @@ package org.jetbrains.kotlin.idea.search
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.FileIndexFacade
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -33,11 +31,10 @@ import com.intellij.psi.search.PsiSearchHelper
 import com.intellij.psi.search.SearchScope
 import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.util.CommonProcessors
-import com.intellij.util.Processor
 import com.intellij.util.indexing.FileBasedIndex
 import org.jetbrains.kotlin.idea.KotlinFileType
-import org.jetbrains.kotlin.idea.util.compat.psiSearchHelperInstance
 import org.jetbrains.kotlin.idea.util.application.runReadAction
+import org.jetbrains.kotlin.idea.util.compat.psiSearchHelperInstance
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtNamedDeclaration
@@ -127,41 +124,17 @@ fun PsiSearchHelper.isCheapEnoughToSearchConsideringOperators(
     return isCheapEnoughToSearch(name, scope, fileToIgnoreOccurrencesIn, progress)
 }
 
-private fun isCheapToSearchUsagesInScripts(scope: GlobalSearchScope, name: String): Boolean {
-    val project = scope.project ?: return true
-
-    var scriptsCount = 0
-    val processor = object : Processor<VirtualFile> {
-        override fun process(file: VirtualFile): Boolean {
-            ProgressManager.checkCanceled()
-            if (findScriptDefinition(file, project) == null) return true
-            return scriptsCount++ < 3
-        }
-    }
-
-    val index = FileIndexFacade.getInstance(project)
-    return runReadAction {
-        FileBasedIndex.getInstance().processFilesContainingAllKeys(
-            IdIndex.NAME,
-            listOf(IdIndexEntry(name, true)),
-            scope,
-            null,
-            { file -> !index.shouldBeFound(scope, file) || processor.process(file) })
-    }
-}
-
 fun findScriptsWithUsages(declaration: KtNamedDeclaration): List<VirtualFile> {
     val project = declaration.project
-    val scope = PsiSearchHelper.SERVICE
-        .getInstance(declaration.project)
-        .getUseScope(declaration) as? GlobalSearchScope
+    val scope = psiSearchHelperInstance(project).getUseScope(declaration) as? GlobalSearchScope
         ?: return emptyList()
 
+    val name = declaration.name.takeIf { it?.isNotBlank() == true } ?: return emptyList()
     val collector = CommonProcessors.CollectProcessor(ArrayList<VirtualFile>())
     runReadAction {
         FileBasedIndex.getInstance().getFilesWithKey(
             IdIndex.NAME,
-            setOf(IdIndexEntry(declaration.name, true)),
+            setOf(IdIndexEntry(name, true)),
             collector,
             scope
         )
