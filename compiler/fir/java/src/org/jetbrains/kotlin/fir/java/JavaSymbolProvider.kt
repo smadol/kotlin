0778274655aeec1bd141f9329830ef6013014a50
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-02-22 18:52:25 +0300

    Connect type enhancement transformer with enhancement infrastructure

--- compiler/fir/java/src/org/jetbrains/kotlin/fir/java/JavaSymbolProvider.kt
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/java/JavaSymbolProvider.kt
@@ -49,13 +49,17 @@ class JavaSymbolProvider(
 
     private val facade: KotlinJavaPsiFacade get() = KotlinJavaPsiFacade.getInstance(project)
 
+    private val symbolProvider by lazy { session.service<FirSymbolProvider>() }
+
     private fun JavaAnnotation.toFirAnnotationCall(): FirAnnotationCall {
+        val annotationClassSymbol = classId?.let { symbolProvider.getClassLikeSymbolByFqName(it) }
         return FirAnnotationCallImpl(
             session, psi = null, useSiteTarget = null,
             annotationTypeRef = FirResolvedTypeRefImpl(
                 session = session,
                 psi = null,
-                type = ConeClassTypeImpl(FirClassSymbol(classId!!), emptyArray(), isNullable = false),
+                type = annotationClassSymbol?.let { ConeClassTypeImpl(it, emptyArray(), isNullable = false) }
+                    ?: ConeClassErrorType("Cannot get class like symbol for annotation $classId"),
                 isMarkedNullable = true,
                 annotations = emptyList()
             )
